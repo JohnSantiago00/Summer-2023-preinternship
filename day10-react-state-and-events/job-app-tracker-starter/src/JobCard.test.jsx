26674ca8738fe78fd46cbaa819da9f669a0717c1
John Santiago <santiago.john96@gmail.com>
2023-06-26 12:15:58 -0400

    day10

---
+++ day10-react-state-and-events/job-app-tracker-starter/src/JobCard.test.jsx
@@ -0,0 +1,17 @@
+// JobCard.test.jsx
+import { render } from "@testing-library/react";
+import { test } from "vitest";
+import JobCard from "./JobCard";
+
+test("renders without crashing", () => {
+  const job = {
+    image: { src: "image.jpg", alt: "job image" },
+    company: "Google",
+    title: "Software Engineer",
+    salary: "100k",
+    location: "New York",
+    postDate: "2022-06-20",
+  };
+
+  render(<JobCard job={job} />);
+});
