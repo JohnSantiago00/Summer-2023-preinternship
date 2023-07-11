70d3fc30e15ff4cd23c510a9af41c6d118abd5ac
John Santiago <santiago.john96@gmail.com>
2023-07-11 09:58:13 -0400

    day19

--- day19-node-and-express-with-postgres/job-application-tracker-api/server.js
+++ day19-node-and-express-with-postgres/job-application-tracker-api/server.js
@@ -1,3 +1,4 @@
+const { query } = require("./database");
 const express = require("express");
 const app = express();
 const port = 4000;
@@ -5,9 +6,10 @@ const jobs = require("./jobs");
 require("dotenv").config();
 
 app.use((req, res, next) => {
+  console.log(`Request: ${req.method} ${req.originalUrl}`);
   res.on("finish", () => {
     // the 'finish' event will be emitted when the response is handed over to the OS
-    console.log(`Request: ${req.method} ${req.originalUrl} ${res.statusCode}`);
+    console.log(`Response status: ${res.statusCode}`);
   });
   next();
 });
@@ -24,59 +26,136 @@ app.get("/", (req, res) => {
 });
 
 // Get all the jobs
-app.get("/jobs", (req, res) => {
-  res.send(jobs);
+app.get("/jobs", async (req, res) => {
+  try {
+    const allJobs = await query("SELECT * FROM job_applications");
+
+    res.status(200).json(allJobs.rows);
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 // Get a specific job
-app.get("/jobs/:id", (req, res) => {
+app.get("/jobs/:id", async (req, res) => {
   const jobId = parseInt(req.params.id, 10);
-  const job = jobs.find((j) => j.id === jobId);
-  if (job) {
-    res.send(job);
-  } else {
-    res.status(404).send({ message: "Job not found" });
+
+  try {
+    const job = await query("SELECT * FROM job_applications WHERE id = $1", [
+      jobId,
+    ]);
+
+    if (job.rows.length > 0) {
+      res.status(200).json(job.rows[0]);
+    } else {
+      res.status(404).send({ message: "Job not found" });
+    }
+  } catch (err) {
+    console.error(err);
   }
 });
 
 // Create a new job
-app.post("/jobs", (req, res) => {
-  const newJob = {
-    ...req.body,
-    id: getNextIdFromCollection(jobs),
-  };
-  jobs.push(newJob);
-  console.log("newJob", newJob);
-  res.status(201).send(newJob);
+app.post("/jobs", async (req, res) => {
+  const {
+    company,
+    title,
+    minSalary,
+    maxSalary,
+    location,
+    postDate,
+    jobPostUrl,
+    applicationDate,
+    lastContactDate,
+    companyContact,
+    status,
+  } = req.body;
+
+  try {
+    const newJob = await query(
+      "INSERT INTO job_applications (company, title, minSalary, maxSalary, location, postDate, jobPostUrl, applicationDate, lastContactDate, companyContact, status) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING *",
+      [
+        company,
+        title,
+        minSalary,
+        maxSalary,
+        location,
+        postDate,
+        jobPostUrl,
+        applicationDate,
+        lastContactDate,
+        companyContact,
+        status,
+      ]
+    );
+
+    res.status(201).json(newJob.rows[0]);
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 // Update a specific job
-app.patch("/jobs/:id", (req, res) => {
+app.patch("/jobs/:id", async (req, res) => {
   const jobId = parseInt(req.params.id, 10);
-  const jobUpdates = req.body;
-  const jobIndex = jobs.findIndex((job) => job.id === jobId);
-  if (jobIndex !== -1) {
-    const originalJob = jobs[jobIndex];
-    const updatedJob = {
-      ...originalJob,
-      ...jobUpdates,
-    };
-    jobs[jobIndex] = updatedJob;
-    res.send(updatedJob);
-  } else {
-    res.status(404).send({ message: "Job not found" });
+
+  const fieldNames = [
+    "company",
+    "title",
+    "minSalary",
+    "maxSalary",
+    "location",
+    "postDate",
+    "jobPostUrl",
+    "applicationDate",
+    "lastContactDate",
+    "companyContact",
+    "status",
+    "jobId",
+  ].filter((name) => req.body[name]);
+
+  let updatedValues = fieldNames.map((name) => req.body[name]);
+  const setValuesSQL = fieldNames
+    .map((name, i) => {
+      return `${name} = $${i + 1}`;
+    })
+    .join(", ");
+
+  try {
+    const updatedJob = await query(
+      `UPDATE job_applications SET ${setValuesSQL} WHERE id = $${
+        fieldNames.length + 1
+      } RETURNING *`,
+      [...updatedValues, jobId]
+    );
+
+    if (updatedJob.rows.length > 0) {
+      res.status(200).json(updatedJob.rows[0]);
+    } else {
+      res.status(404).send({ message: "Job not found" });
+    }
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+    console.error(err);
   }
 });
 
 // Delete a specific job
-app.delete("/jobs/:id", (req, res) => {
+app.delete("/jobs/:id", async (req, res) => {
   const jobId = parseInt(req.params.id, 10);
-  const jobIndex = jobs.findIndex((job) => job.id === jobId);
-  if (jobIndex !== -1) {
-    jobs.splice(jobIndex, 1);
-    res.send({ message: "Job deleted successfully" });
-  } else {
-    res.status(404).send({ message: "Job not found" });
+
+  try {
+    const deleteOp = await query("DELETE FROM job_applications WHERE id = $1", [
+      jobId,
+    ]);
+
+    if (deleteOp.rowCount > 0) {
+      res.status(200).send({ message: "Job deleted successfully" });
+    } else {
+      res.status(404).send({ message: "Job not found" });
+    }
+  } catch (err) {
+    console.error(err);
   }
 });
 
